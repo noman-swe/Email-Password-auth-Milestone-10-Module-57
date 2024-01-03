316785353a33877496e41a2e3db539e15a87303b
noman-swe <noman.swe130@gmail.com>
2024-01-04 00:28:56 +0600

    57-2 Simple Form, Input, Change, Blur, Submit, PreventDefault

--- src/App.js
+++ src/App.js
@@ -4,11 +4,25 @@ import {getAuth} from 'firebase/auth';
 
 const auth = getAuth(app);
 function App() {
+  
+  const handleEmailBlur = event => {
+    console.log(event.target.value);
+  }
+  
+  const handlePassBlur = event => {
+    console.log(event.target.value);
+  }
+  const handleFormSubmit = (event) => {
+    console.log('Clicked');
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
-     <form>
-      <input type="text" placeholder='Email' />
-      <input type="password" name="" id="" placeholder='Password'/>
+     <form style={{'marginTop': '20px'}}>
+      <input onBlur={handleEmailBlur} type="text" placeholder='Email' /> <br />
+      <input onBlur={handlePassBlur} type="password" name="" id="" placeholder='Password'/> <br />
+      <input onClick={handleFormSubmit} type="submit" value="Log in" />
      </form>
     </div>
   );
